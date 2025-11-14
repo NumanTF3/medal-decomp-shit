ea98eb3bc01ed18b261d14bc639480b09b53c089
Numan <numantf2@gmail.com>
2025-11-14 20:45:38 +0500

    Update autoexec.lua

--- autoexec.lua
+++ autoexec.lua
@@ -1,4 +1,6 @@
 getgenv().decompile = function(script_instance)
+  local request = (syn and syn.request) or (http and http.request) or http_request or (fluxus and fluxus.request) or request;
+  local base64Encode = base64_encode or crypt and crypt.base64.encode
   local bytecode = getscriptbytecode(script_instance)
   local encoded = crypt.base64encode(bytecode)
   return request(
